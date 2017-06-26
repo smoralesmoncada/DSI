f00683351268b4809674ff5102f294364ed0691e
JavierRD14 <javier0194@gmail.com>
2017-06-26 19:12:55 -0400

    Update db.php

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=postgres',
-    'username' => 'root',
-    'password' => '',
+    'username' => 'postgres',
+    'password' => 'admin123',
     'charset' => 'utf8',
 ];
