0a2b8548ffd88274cbf7942509685d2e00957327
Gabriel <vagrant@wheezy.raw>
2017-06-27 17:07:25 +0000

    Commit Inicial

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'pgsql:host=localhost;dbname=postgres',
+    'username' => 'postgres',
+    'password' => '1234',
+    'charset' => 'utf8',
+];
